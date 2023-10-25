9e8dedd74d341ddc13ee507543479bd99fa670cf
IlvaKanani <ilvakanani29@yahoo.com>
2023-10-25 14:42:39 +0200

    first commit

---
+++ Controllers/StaffsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROJECT.Controllers
+{
+    public class StaffsController : Controller
+    {
+        //private readonly IStaffsService staffsService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
